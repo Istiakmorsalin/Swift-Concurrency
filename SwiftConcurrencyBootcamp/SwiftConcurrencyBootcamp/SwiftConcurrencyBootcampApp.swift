d6c18f1a41c437d1722a6e080f27f879b28b8dea
Istiak Morsalin <anik.cse@gmail.com>
2024-05-28 22:14:38 -0400

    another commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Istiak Morsalin on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
